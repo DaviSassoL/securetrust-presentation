9f2c22a660ee31021af5b672c6b8af17ccf8b3a7
DaviSassoL <davilima39@hotmail.com>
2025-06-14 11:31:41 -0300

    Create App.jsx

---
+++ public/src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+// Componente raiz que importa e exibe a apresentação
+import React from 'react';
+import Presentation from './components/Presentation';
+
+function App() {
+  return <Presentation />;
+}
+
+export default App;
